368f2cc803d7d89097d926845c96b0f3543a5952
gjlld <gareth.davies@londonmet.ac.uk>
2016-03-08 17:05:28 +0000

    This is just a test to see if git works with ssh from Atom

--- Analog.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-
-int main(int argc, char const *argv[]) {
-
-  printf("This is just a test!%s\n", );
-
-  return 0;
-}
---
+++ Test.c
@@ -0,0 +1,6 @@
+int main(int argc, char const *argv[]) {
+
+  printf("This is a test.%s\n", );
+  
+  return 0;
+}
