11ddba74d2e30c900960b852cd47a097672d9278
gjlld <gareth.davies@londonmet.ac.uk>
2016-03-10 14:52:03 +0000

    Edited bash script

--- copy-make-run.sh
+++ copy-make-run.sh
@@ -2,5 +2,5 @@
 
 # change the <user>@<address> below to suit your setup
 
-scp -r Makefile hello-world-OpenVG.c pi@192.168.1.103
+scp -r Makefile hello-world-OpenVG.c pi@192.168.1.103:/home/pi/
 ssh pi@192.168.1.103 && make
